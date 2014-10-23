9eb3d7dcc52c9c7199f403bab5f1998a00bf41f6
João Gabriel Esteves <joao.esteves@tse.jus.br>
2014-10-23 16:38:25 -0200

    Integração entre Spring e Freemarker.

--- src/main/java/br/jus/tse/testespring/beans/service/QualquerService.java
+++ src/main/java/br/jus/tse/testespring/beans/service/QualquerService.java
@@ -1,8 +1,5 @@
 package br.jus.tse.testespring.beans.service;
 
-import java.io.File;
-import java.io.StringWriter;
-import java.io.Writer;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,13 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerConfigurationFactoryBean;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import br.jus.tse.testespring.beans.mail.ProcbatchMailSender;
-import freemarker.template.Configuration;
-import freemarker.template.DefaultObjectWrapper;
-import freemarker.template.Template;
-import freemarker.template.TemplateExceptionHandler;
-import freemarker.template.Version;
 
 @Service
 public class QualquerService {
@@ -27,14 +21,22 @@ public class QualquerService {
 	@Autowired
 	private JavaMailSender mailSender;
 	
+	@Autowired
+	private FreeMarkerConfigurationFactoryBean freeMarkerConfigurationFactory;
+	
 	public void executarJob() {
 		
 		try {
+			Map<String, String> root = new HashMap<String, String>();
+			root.put("titulo", "Se vocÃª Ã© jovem ainda");
+			// TODO parametrizar template tambem
+			String html = FreeMarkerTemplateUtils.processTemplateIntoString(freeMarkerConfigurationFactory.getObject().getTemplate("template-outlook2007.ftl"), root);
+			
 			MimeMessageHelper message = new MimeMessageHelper(mailSender.createMimeMessage());
 			message.setFrom("secad@tse.jus.br", "Secad - Procbatch");
 			message.setTo("joao.esteves@tse.jus.br");
-			message.setSubject("Execução de Anomalias da Filiação Partidária");
-			message.setText(getHTML(), true);
+			message.setSubject("ExecuÃ§Ã£o de Anomalias da FiliaÃ§Ã£o PartidÃ¡ria");
+			message.setText(html, true);
 			
 			mailSender.send(message.getMimeMessage());
 		} catch (Exception e) {
@@ -43,14 +45,14 @@ public class QualquerService {
 		
 	}
 	
-	private String getHTML() throws Exception {
-		/*
+	/*private String getHTML() throws Exception {
+		
 		 * ----------------------------------------------------------------------
 		 * -
-		 */
-		/* You should do this ONLY ONCE in the whole application life-cycle: */
+		 
+		 You should do this ONLY ONCE in the whole application life-cycle: 
 
-		/* Create and adjust the configuration */
+		 Create and adjust the configuration 
 		Configuration cfg = new Configuration();
 
 //		cfg.setDirectoryForTemplateLoading(new File("C:\\env-dev\\git\\spring-mail-sender\\src\\main\\resources"));
@@ -60,29 +62,29 @@ public class QualquerService {
 		cfg.setTemplateExceptionHandler(TemplateExceptionHandler.HTML_DEBUG_HANDLER);
 		cfg.setIncompatibleImprovements(new Version(2, 3, 20));
 
-		/*
+		
 		 * ----------------------------------------------------------------------
 		 * -
-		 */
-		/* You usually do these for many times in the application life-cycle: */
+		 
+		 You usually do these for many times in the application life-cycle: 
 
-		/* Create a data-model */
+		 Create a data-model 
 		Map root = new HashMap();
-		root.put("titulo", "Inconsistências do Filiaweb");
-		/*Map latest = new HashMap();
+		root.put("titulo", "InconsistÃªncias do Filiaweb");
+		Map latest = new HashMap();
 		root.put("latestProduct", latest);
 		latest.put("url", "products/greenmouse.html");
-		latest.put("name", "green mouse");*/
+		latest.put("name", "green mouse");
 
-		/* Get the template */
+		 Get the template 
 		Template temp = cfg.getTemplate("template-outlook2007.ftl");
 
-		/* Merge data-model with template */
+		 Merge data-model with template 
 //		Writer out = new OutputStreamWriter(System.out);
 		Writer out = new StringWriter();
 		temp.process(root, out);
 		
 		return out.toString();
-	}
+	}*/
 	
 }
